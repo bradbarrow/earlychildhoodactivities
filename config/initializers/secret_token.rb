02c75ae99f4d6351478788fa869094a2c11dcddb
Brad Barrow <bradbarrow@Brads-MacBook-Pro.local>
2012-07-24 21:37:44 +1000

    Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eca::Application.config.secret_token = '0c79f2543c45dc1f6fe9bcbc4259b8ba5574a12a6f550c1945fb4bfdbb4d193aa82104f91d1f21ebfacfdaa6b8b2645db3db764a64560592ca2aa85b980c383b'
